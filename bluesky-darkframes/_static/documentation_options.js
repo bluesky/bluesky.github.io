442b46913bef6cdbd08e0dc9c5b841ce5a75735e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-03-19 00:21:12 +0000

    Update docs after building Travis build 248 of bluesky/bluesky-darkframes
    The docs were built from the branch 'master' against the commit
    2aa171cfaf9e9b617319b032ab361392e4bfe817.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/bluesky-darkframes/jobs/664188970.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --deploy-repo bluesky/bluesky.github.io --deploy-branch-name master bluesky-darkframes

--- bluesky-darkframes/_static/documentation_options.js
+++ bluesky-darkframes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.post44+g762ced1',
+    VERSION: '0.3.0.post48+g2aa171c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
