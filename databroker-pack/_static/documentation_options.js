9d41624cdbe046071a1bddd80b92f8fa6ba1d614
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-05 02:28:13 +0000

    Update docs after building Travis build 72 of bluesky/databroker-pack
    The docs were built from the branch 'master' against the commit
    a27dbf3660542e7da36412efdf6ece949c1effc5.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/databroker-pack/jobs/683206121.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html/ --deploy-repo bluesky/bluesky.github.io --deploy-branch-name master databroker-pack

--- databroker-pack/_static/documentation_options.js
+++ databroker-pack/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.4.post5+g7be55f6',
+    VERSION: '0.1.4.post33+ga27dbf3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
