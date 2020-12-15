efaa6e8af4322c92ba6d7a4b49317d71b107eaae
danielballan <danielballan@users.noreply.github.com>
2020-12-15 22:49:33 +0000

    deploy: bluesky/databroker@dd38983a312b2577f758dca12661ebfe4e32289a

--- databroker/_static/documentation_options.js
+++ databroker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.0b1.post29+gfee6bb48',
+    VERSION: '1.2.0b1.post33+gdd38983a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
