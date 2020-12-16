eb8c172ce1a9a496ff592af2e115008545527197
danielballan <danielballan@users.noreply.github.com>
2020-12-16 00:26:40 +0000

    deploy: bluesky/area-detector-handlers@46b0703aae9ef1126339dba144e269d47fe52bfa

--- area-detector-handlers/_static/documentation_options.js
+++ area-detector-handlers/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.7.post7+g23584ac',
+    VERSION: '0.0.7.post9+g46b0703',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
