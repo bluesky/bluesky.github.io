6a33f3cf8eca79455875bb7c956f38439f625caf
dylanmcreynolds <dylanmcreynolds@users.noreply.github.com>
2020-12-17 21:58:37 +0000

    deploy: bluesky/databroker@354caad6a308c69aebbc8f2420d7eb96bc6eb0ed

--- databroker/_static/documentation_options.js
+++ databroker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.0b1.post45+gde8601cc',
+    VERSION: '1.2.0b1.post47+g354caad6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
