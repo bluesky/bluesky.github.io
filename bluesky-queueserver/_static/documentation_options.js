c877b31ed2edc5ddd851ffe38545a0d8dee65283
mrakitin <mrakitin@users.noreply.github.com>
2020-12-24 17:42:41 +0000

    deploy: bluesky/bluesky-queueserver@ae2f3bcf055fc29d8748506e8abd74d8d8eb6fb9

--- bluesky-queueserver/_static/documentation_options.js
+++ bluesky-queueserver/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g2430e00',
+    VERSION: '0.post1+gae2f3bc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
