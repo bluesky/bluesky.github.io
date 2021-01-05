c4d78f069cd3a9f1fcddbb2c1136a089fd21d6e2
danielballan <danielballan@users.noreply.github.com>
2021-01-05 21:09:59 +0000

    deploy: bluesky/bluesky-queueserver@84c35cfe46a86ab0b08b15054ed0d85fd480b294

--- bluesky-queueserver/_static/documentation_options.js
+++ bluesky-queueserver/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g2a84c01',
+    VERSION: '0.post1+g84c35cf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
