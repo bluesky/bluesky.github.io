7bab42ca7a5001bcf9a0ee448bf09a5c246ee3ea
Dan Allan <dallan@bnl.gov>
2021-01-22 18:02:13 -0500

    Update rewrite draft.

--- databroker/docs-rewrite-draft/_static/documentation_options.js
+++ databroker/docs-rewrite-draft/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.0+29.g8618a13e',
+    VERSION: '1.2.0+38.g9070eecf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
