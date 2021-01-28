08eacf35c70af1639c8756bcfdd1a1b9ce3205de
JulReinhardt <JulReinhardt@users.noreply.github.com>
2021-01-28 16:07:15 +0000

    deploy: bluesky/bluesky-widgets@6a79bc409c60323c4742fcde17af75fb5dd85219

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6b1.post9+gfe67163',
+    VERSION: '0.0.6b1.post14+g6a79bc4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
