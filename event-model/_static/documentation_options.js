174c3e8423b2b471e8f182b0a64703c9b201ba06
stuartcampbell <stuartcampbell@users.noreply.github.com>
2021-01-29 15:02:17 +0000

    deploy: bluesky/event-model@c90c5f12a2c615c5c26d5f493cc17405f9d94486

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.17.0.post5+gebffe8e',
+    VERSION: '1.17.0.post7+gc90c5f1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
