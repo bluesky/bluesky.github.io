902d5d058926b175d0506b6e4a91b0f4dd5adc2c
danielballan <danielballan@users.noreply.github.com>
2021-02-03 03:21:32 +0000

    deploy: bluesky/bluesky@0a91b8b8a52509198ed5b884c208d58601424828

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.7.post19+g4d8240e1',
+    VERSION: '1.6.7.post23+g0a91b8b8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
