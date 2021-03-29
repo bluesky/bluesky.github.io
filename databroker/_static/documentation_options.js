439aadd94c433ca3067acae03c8d20041a9dcdb8
gwbischof <gwbischof@users.noreply.github.com>
2021-03-29 19:14:38 +0000

    deploy: bluesky/databroker@b2ab1bb0368975ad862d223c24c6f66a38063a40

--- databroker/_static/documentation_options.js
+++ databroker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.2.post13+g13bfca28',
+    VERSION: '1.2.2.post21+gb2ab1bb0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
