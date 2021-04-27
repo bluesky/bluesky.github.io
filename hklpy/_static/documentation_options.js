8603301e28b78e69a51c899781f640551265877c
mrakitin <mrakitin@users.noreply.github.com>
2021-04-27 19:16:22 +0000

    deploy: bluesky/hklpy@7fdad1b5a8aa33041fa99c765a88326ccdb9a3ce

--- hklpy/_static/documentation_options.js
+++ hklpy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g0c5d364.dirty',
+    VERSION: '0+untagged.1.g7fdad1b.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
