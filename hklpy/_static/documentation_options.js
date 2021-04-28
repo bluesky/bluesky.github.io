829856153bfbe6efef45dd3c133bd59902d49679
prjemian <prjemian@users.noreply.github.com>
2021-04-28 17:37:11 +0000

    deploy: bluesky/hklpy@af126bdc60cca472a9fd3d4774555d77f8e341dc

--- hklpy/_static/documentation_options.js
+++ hklpy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g23a586c.dirty',
+    VERSION: '0+untagged.1.gaf126bd.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
