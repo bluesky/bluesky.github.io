fbefd712698ee962ea1f72e47611fc8cf9f33256
danielballan <danielballan@users.noreply.github.com>
2021-05-22 01:43:09 +0000

    deploy: bluesky/tiled@4adf8cedf900097581995db57271947b71bf746d

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a10.post53+gd43386f',
+    VERSION: '0.1.0a10.post54+g4adf8ce',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
