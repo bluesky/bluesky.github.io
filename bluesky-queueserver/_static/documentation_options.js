f37db91b98dfdd48f16f999a5f564d61e072f7e6
dmgav <dmgav@users.noreply.github.com>
2021-06-04 22:56:18 +0000

    deploy: bluesky/bluesky-queueserver@c06c0de7f43eed6d0cb6cb42da523d34931556c7

--- bluesky-queueserver/_static/documentation_options.js
+++ bluesky-queueserver/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g5e59302',
+    VERSION: '0.post1+gc06c0de',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
