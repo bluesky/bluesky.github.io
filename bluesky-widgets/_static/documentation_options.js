148d67cfd74a3ed1445b10d2f74971ec7e160ccd
AbbyGi <AbbyGi@users.noreply.github.com>
2021-06-15 14:08:14 +0000

    deploy: bluesky/bluesky-widgets@cc7668ccab83b62e9d0b0bea50b1857e222041cd

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.8.post122+gfbdb2e5',
+    VERSION: '0.0.8.post125+gcc7668c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
