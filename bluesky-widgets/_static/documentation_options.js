27b200b84553cc5ded06ed70e37482d420e0f416
gwbischof <gwbischof@users.noreply.github.com>
2021-07-12 21:16:43 +0000

    deploy: bluesky/bluesky-widgets@62c08952f86a4ac885227f65fb95f72340c90700

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.8.post138+g4863997',
+    VERSION: '0.0.8.post140+g62c0895',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
