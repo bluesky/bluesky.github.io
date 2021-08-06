e160ac1eb8f1b37ed2e514eb53b72c7077ece847
jmaruland <jmaruland@users.noreply.github.com>
2021-08-06 14:36:06 +0000

    deploy: bluesky/tiled@5fc5ea6d470e43c7377c29bfc42ba1dde47cc159

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a28.post45+g4b1bdfe',
+    VERSION: '0.1.0a28.post47+g5fc5ea6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
