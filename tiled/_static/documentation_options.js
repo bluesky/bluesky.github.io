7b102de8ebd037b64ae146c13674274d19963f93
dylanmcreynolds <dylanmcreynolds@users.noreply.github.com>
2021-08-17 20:45:08 +0000

    deploy: bluesky/tiled@a9ef862b226f3d0c65d4351680bec3a6404538e6

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a28.post60+gb667a9e',
+    VERSION: '0.1.0a28.post69+ga9ef862',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
