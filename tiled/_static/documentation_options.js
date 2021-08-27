d5b037a4d64e601cb51e4d2fb18ee4d3efaf05f8
kleinhenz <kleinhenz@users.noreply.github.com>
2021-08-27 19:53:48 +0000

    deploy: bluesky/tiled@6531d7f3dd1634562b7d2b9574f8a7071b6c8de8

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a30.post42+g62bd3ac',
+    VERSION: '0.1.0a30.post47+g6531d7f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
