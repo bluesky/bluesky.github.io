bab4ba6a966d1bfc1d0e1bc8eb244a12e889c37f
mrakitin <mrakitin@users.noreply.github.com>
2021-09-09 11:41:46 +0000

    deploy: bluesky/bluesky@45e5459b51a900c6d31b9569a25ccf3e7561c570

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.0.post23+gb13003df',
+    VERSION: '1.7.0.post26+g45e5459b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
