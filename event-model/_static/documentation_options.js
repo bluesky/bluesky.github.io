530bed203f192fed0c78d04134fb8c26d98b7fec
jklynch <jklynch@users.noreply.github.com>
2021-10-22 15:12:40 +0000

    deploy: bluesky/event-model@701cd8f2486fc341eb12445762e77c4ae3c07ee7

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.17.1.post9+gf519237',
+    VERSION: '1.17.2.post3+g701cd8f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
