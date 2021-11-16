5ffcb183a9327c88784de3d7d7ab086055cf0249
jklynch <jklynch@users.noreply.github.com>
2021-11-16 22:18:12 +0000

    deploy: bluesky/bluesky@1f277a044e5b23ae5f98c86d77c3871b4c9a1dc5

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.1.post7+g98af07ce',
+    VERSION: '1.8.1.post9+g1f277a04',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
