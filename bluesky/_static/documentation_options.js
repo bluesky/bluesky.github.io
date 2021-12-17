367efc6f0ee97d64a4f41933cf80d4b0007def46
ZLLentz <ZLLentz@users.noreply.github.com>
2021-12-17 20:47:33 +0000

    deploy: bluesky/bluesky@eb08bb68a454b1ed90e8ef57b03c0e99ab09ab08

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.1.post18+g8ef9005a',
+    VERSION: '1.8.1.post20+geb08bb68',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
