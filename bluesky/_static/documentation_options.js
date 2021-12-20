20866db8e50719060137ef6cd06e98c8682a28ce
tacaswell <tacaswell@users.noreply.github.com>
2021-12-20 20:08:43 +0000

    deploy: bluesky/bluesky@2acf8357e49ce579ac4d59b0c4d651ae949530ad

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.1.post20+geb08bb68',
+    VERSION: '1.8.1.post22+g2acf8357',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
