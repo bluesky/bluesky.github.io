01c0a5cbd0b3d30b2b4eaa7858fbe475a9b7c55c
mrakitin <mrakitin@users.noreply.github.com>
2022-01-21 21:03:57 +0000

    deploy: bluesky/event-model@54d8907783f0cce479183d4c6fe625affe8566b7

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.17.2.post5+gdb98932',
+    VERSION: '1.17.2.post7+g54d8907',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
