382e1d7c394bd206c192c8827ba96e100eb9d038
dmgav <dmgav@users.noreply.github.com>
2022-04-25 16:12:00 +0000

    deploy: bluesky/bluesky-widgets@19e823f3d57e26d32e475eb49bfd68e1c16c531a

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.12',
+    VERSION: '0.0.12.post3+g19e823f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
