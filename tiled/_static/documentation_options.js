70e8bbf4a8d98098d506dacbb289028b4af630ef
cryos <cryos@users.noreply.github.com>
2022-06-13 17:50:26 +0000

    deploy: bluesky/tiled@2b35ba77b13ed9a023010f4c5346100d189a5722

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a64.post1+gc3b032e',
+    VERSION: '0.1.0a64.post4+g2b35ba7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
