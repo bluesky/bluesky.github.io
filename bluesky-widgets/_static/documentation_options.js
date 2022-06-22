1152e6e0f5c4780e66ca5875f79621732780e318
tacaswell <tacaswell@users.noreply.github.com>
2022-06-22 03:48:22 +0000

    deploy: bluesky/bluesky-widgets@8f5e8cec0cd08f6f3e6e17a3467818c85e643f56

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.12.post12+gfacc454',
+    VERSION: '0.0.12.post14+g8f5e8ce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
