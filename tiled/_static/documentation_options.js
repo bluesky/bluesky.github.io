c2ee3f4e075780db38f943a6c7b18fc19b4f01fe
jklynch <jklynch@users.noreply.github.com>
2022-07-17 21:19:04 +0000

    deploy: bluesky/tiled@ee067ddeec2c13731e15d80fa03cc802c9364b49

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a65.post4+gc7f3460',
+    VERSION: '0.1.0a65.post6+gee067dd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
