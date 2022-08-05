52c72342b475c172c8a714ea82fcace2795572bd
jklynch <jklynch@users.noreply.github.com>
2022-08-05 14:05:36 +0000

    deploy: bluesky/event-model@82e233c02026155e0aa2bd702550405fa96c0ff5

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.17.2.post12+g40a38c9',
+    VERSION: '1.18.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
