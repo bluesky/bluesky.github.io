18a15ebbf1a54c6e4b277e2530404a28671398f4
danielballan <danielballan@users.noreply.github.com>
2022-08-25 01:30:16 +0000

    deploy: bluesky/event-model@4c6df2cc062b6641250436b57e5e933aa3a5f7cd

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.18.0',
+    VERSION: '1.18.0.post2+g4c6df2c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
