4f203f6732b3c40348124ec5e42f745c66cb74cf
dmgav <dmgav@users.noreply.github.com>
2022-10-31 20:29:25 +0000

    deploy: bluesky/bluesky-httpserver@593da615468930f4c8cb0bcb737300db86513966

--- bluesky-httpserver/_static/documentation_options.js
+++ bluesky-httpserver/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g48d2a5e',
+    VERSION: '0.post1+g593da61',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
