8a1ea1da430145ead783862f20f4088074366a49
tacaswell <tacaswell@users.noreply.github.com>
2022-11-03 17:28:36 +0000

    deploy: bluesky/event-model@eac55ca21e54328a7fc1f16fea71e56460ed6a58

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.18.0.post5+g772ae3c',
+    VERSION: '1.18.0.post28+geac55ca',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
