8a831e079f3bdc91be63ad94c656defd813671fe
untzag <untzag@users.noreply.github.com>
2023-02-08 20:14:03 +0000

    deploy: bluesky/bluesky@a60e5c7323a61b9a988ce9f8d1b4d7f4ac495ee8

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.10.0.post20+gd46eed87',
+    VERSION: '1.10.0.post21+ga60e5c73',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
