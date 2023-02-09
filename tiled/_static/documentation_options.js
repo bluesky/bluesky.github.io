6f69414b4b6f9cff5e53961b2aee80176585e642
mrakitin <mrakitin@users.noreply.github.com>
2023-02-09 15:39:11 +0000

    deploy: bluesky/tiled@0404aedf95edc20edd4a4541183ebbb4a0b38669

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a83.post10+g313ec84',
+    VERSION: '0.1.0a83.post13+g0404aed',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
