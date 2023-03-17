29de1f71cc9f44bfcb629c90fdb2fe37026031b2
tacaswell <tacaswell@users.noreply.github.com>
2023-03-17 19:56:48 +0000

    deploy: bluesky/tiled@5cef17f42b1cf87078014e997b3cecea7ab0a4e9

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a85.post21+g616e636',
+    VERSION: '0.1.0a85.post24+g5cef17f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
