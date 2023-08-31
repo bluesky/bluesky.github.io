f2cea05c00d305a90b8266025353d0f645f3860c
prjemian <prjemian@users.noreply.github.com>
2023-08-31 21:52:47 +0000

    deploy: bluesky/tiled@ca9acdc1384dba3672ac83ea8a845f280dce5fb5

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a106.dev1+gb356456',
+    VERSION: '0.1.0a106.dev4+gca9acdc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
