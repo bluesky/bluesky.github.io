6e97baa2d7d24428f7b5998a31ab15b733ca4258
prjemian <prjemian@users.noreply.github.com>
2023-09-04 16:31:16 +0000

    deploy: bluesky/bluesky@8deba753a63de84a76b1a9aceb9edf3ae3268e1c

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.0.post24+g19a528bd',
+    VERSION: '1.11.0.post25+g8deba753',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
