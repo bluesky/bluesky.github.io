4c5266c8b3610c00a17260ebc799025a1f988cfb
evalott100 <evalott100@users.noreply.github.com>
2023-09-14 08:32:47 +0000

    deploy: bluesky/bluesky@11b11bcd5f429e96419d6fdb282631e00e571d28

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.0.post28+g4dcc51c6',
+    VERSION: '1.11.0.post31+g11b11bcd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
