1682f7d44274ddf581113af7124ac22687d06885
rosesyrett <rosesyrett@users.noreply.github.com>
2023-10-20 09:36:51 +0000

    deploy: bluesky/bluesky@ac0f5a3640e73ae5edb4398d94c938234da057bb

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.0.post70+g880dbdf2',
+    VERSION: '1.11.0.post72+gac0f5a36',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
