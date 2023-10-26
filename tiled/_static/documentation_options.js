539703d998015bb4397ab3ce02f75bf0a4c40c95
AbbyGi <AbbyGi@users.noreply.github.com>
2023-10-26 12:59:18 +0000

    deploy: bluesky/tiled@81804e1361456bd76d3a9919a56750e3720c5c5c

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a109.dev1+g7c122f9',
+    VERSION: '0.1.0a109.dev10+g81804e1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
