e8d615331a5d87af8717800b74dba38a96c9ef09
Wiebke <Wiebke@users.noreply.github.com>
2023-10-26 22:18:04 +0000

    deploy: bluesky/tiled@1db4a8b8fe1fea195adb406fb13e9e3eede7cf96

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a109.dev10+g81804e1',
+    VERSION: '0.1.0a109.dev16+g1db4a8b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
