1a645433c4b8aeca9e301eb4682d37f920563eb0
padraic-shafer <padraic-shafer@users.noreply.github.com>
2023-11-16 05:21:54 +0000

    deploy: bluesky/tiled@5df70f8178acde7e8b3a9a6abb28c85b29ffd391

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a110.dev6+gebfa643',
+    VERSION: '0.1.0a110.dev10+g5df70f8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
