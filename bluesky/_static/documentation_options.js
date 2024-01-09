c62567a815a0f79330d4d1dd0a2df86089e25dd1
coretl <coretl@users.noreply.github.com>
2024-01-09 10:01:55 +0000

    deploy: bluesky/bluesky@0943efdc9ba8ecff9b5c59cb0f5a38d7bb8ef1c3

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.12.0.post1+g9b0d7295',
+    VERSION: '1.12.0.post12+g0943efdc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
