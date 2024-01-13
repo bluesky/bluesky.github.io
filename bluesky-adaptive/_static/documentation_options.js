d0c92449d129b6e2e60ccdd12b98d12e896d4076
maffettone <maffettone@users.noreply.github.com>
2024-01-13 16:19:22 +0000

    deploy: bluesky/bluesky-adaptive@c5a89ce072d67bc88a95e6c5cdce59a493a044c5

--- bluesky-adaptive/_static/documentation_options.js
+++ bluesky-adaptive/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gf21d6ff',
+    VERSION: '0.post1+gc5a89ce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
