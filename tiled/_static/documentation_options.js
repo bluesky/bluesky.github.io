33a35c8fe66db43ceff78d4cd2e867c20cdec3c2
Kezzsim <Kezzsim@users.noreply.github.com>
2024-04-26 19:17:08 +0000

    deploy: bluesky/tiled@f24b3922cca08c5c2726c5e3bf907420e77c4621

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a121.dev1+gf44177e',
+    VERSION: '0.1.0a121.dev5+gf24b392',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
