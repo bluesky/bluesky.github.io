b5bf2e382e677a7a53a22d4f4ac6e0658c5fb905
gwbischof <gwbischof@users.noreply.github.com>
2024-07-15 20:21:23 +0000

    deploy: bluesky/tiled@9398939207e4a1d61d15ba56a63d4841cba8decf

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b6.dev1+g356dbab0',
+    VERSION: '0.1.0b6.dev5+g93989392',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
