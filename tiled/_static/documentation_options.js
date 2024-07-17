292b3f7db6dc0228f64e4b2eb1aa526e17930015
Kezzsim <Kezzsim@users.noreply.github.com>
2024-07-17 19:05:28 +0000

    deploy: bluesky/tiled@2e392d6dda3cf065b10e2df44df9f5b10f0a781f

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b6.dev5+g93989392',
+    VERSION: '0.1.0b6.dev8+g2e392d6d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
