b0987604957cec8fa407c89d88032103e893d391
tacaswell <tacaswell@users.noreply.github.com>
2025-01-10 17:37:39 +0000

    deploy: bluesky/tiled@6314f1d42d4ca41643f6fee834b5129056fde130

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b14.dev1+gcc219987',
+    VERSION: '0.1.0b14.dev11+g6314f1d4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
