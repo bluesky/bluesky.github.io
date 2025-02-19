17ae26f0ff09dc97cff7d3361597efeb03850e60
padraic-shafer <padraic-shafer@users.noreply.github.com>
2025-02-19 18:03:13 +0000

    deploy: bluesky/tiled@cc854bb6aa729c0f6cd0ec001f1d81702106e56d

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b19.dev3+g37191358',
+    VERSION: '0.1.0b19.dev6+gcc854bb6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
