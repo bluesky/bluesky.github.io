dc812b257750b45587f73de6789cb152ebda71da
dmgav <dmgav@users.noreply.github.com>
2025-04-22 17:42:17 +0000

    deploy: bluesky/bluesky-queueserver@e1369d40bd0bf60f52411c1d593b9d7940a2a64c

--- bluesky-queueserver/_static/documentation_options.js
+++ bluesky-queueserver/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.21.post27+g29dc8c6',
+    VERSION: '0.0.21.post29+ge1369d4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
