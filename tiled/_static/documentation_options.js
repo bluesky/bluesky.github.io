c3203367efccbd768cf136b75e1c3fe887928ea4
genematx <genematx@users.noreply.github.com>
2025-05-05 20:19:21 +0000

    deploy: bluesky/tiled@78a98cf7cdf1dd43a7c3d05f9ee4ce2241fefc67

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b24.dev21+g5bc34623',
+    VERSION: '0.1.0b24.dev24+g78a98cf7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
