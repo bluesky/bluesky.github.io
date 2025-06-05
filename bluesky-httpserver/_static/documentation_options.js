8a25d58e438eea27a63309e1b5a61cfb9a8b7521
dmgav <dmgav@users.noreply.github.com>
2025-06-05 13:13:27 +0000

    deploy: bluesky/bluesky-httpserver@5215bf960c50d93550f88f1df90f834a03c98a36

--- bluesky-httpserver/_static/documentation_options.js
+++ bluesky-httpserver/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.12.post14+ge3d7f22',
+    VERSION: '0.0.12.post17+g5215bf9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
