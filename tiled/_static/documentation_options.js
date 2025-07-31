a2e431b1043de8a49b4190686c807635da6cd19d
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2025-07-31 14:50:32 +0000

    deploy: bluesky/tiled@406ea84e33b177a070bb02361c6f2c90c0546bdc

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b31.dev6+gcd386219',
+    VERSION: '0.1.0b31.dev7+g406ea84e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
