1ac2bf2df64e2fc293303df0d7f261d65222faab
danielballan <danielballan@users.noreply.github.com>
2025-08-27 02:21:28 +0000

    deploy: bluesky/tiled@d98cde208e2fa2e8d59583ee63e33c71dc57f594

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.1.dev2313+g277ec5a37',
+    VERSION: '0.0.1.dev2313+gd98cde208',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
