6159956549822887f93e48af91ea413c4d924c42
padraic-shafer <padraic-shafer@users.noreply.github.com>
2025-12-06 22:56:09 +0000

    deploy: bluesky/tiled@440130bdd5ac537f1bec4eb4325c2d8e1a852f97

--- tiled/_static/documentation_options.js
+++ tiled/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.1.dev2376+gfb08a1971',
+    VERSION: '0.0.1.dev2377+g440130bdd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
