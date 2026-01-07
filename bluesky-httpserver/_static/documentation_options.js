fde6d17116b939c5ae2747c09d8f4df3f7202296
dmgav <dmgav@users.noreply.github.com>
2026-01-07 14:19:12 +0000

    deploy: bluesky/bluesky-httpserver@be0025bacc5e5cb7d132161e9181d35b2f07da6a

--- bluesky-httpserver/_static/documentation_options.js
+++ bluesky-httpserver/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.12.post37+g39a1662',
+    VERSION: '0.0.12.post39+gbe0025b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
