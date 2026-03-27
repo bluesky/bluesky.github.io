268c004b1f67c5221e20b4a460d8cd8fdfbf11a0
jwlodek <jwlodek@users.noreply.github.com>
2026-03-27 14:45:23 +0000

    deploy: bluesky/bluesky-widgets@84bf5a993b3fd051eec3dee1ca242cfa324dcfc2

--- bluesky-widgets/_static/documentation_options.js
+++ bluesky-widgets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.16.post19+g99ebf7e',
+    VERSION: '0.0.16.post23+g84bf5a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
