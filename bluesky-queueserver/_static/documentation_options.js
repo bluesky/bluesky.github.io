f03e31431b0b31518c20259bcdc9c3d234f8cae7
thopkins32 <thopkins32@users.noreply.github.com>
2026-09-04 19:07:10 +0000

    deploy: bluesky/bluesky-queueserver@f65224d73733039a15ca17185178bfb366b68ece

--- bluesky-queueserver/_static/documentation_options.js
+++ bluesky-queueserver/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.26.dev37+gb787bbf00',
+    VERSION: '0.0.26.dev39+gf65224d73',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
